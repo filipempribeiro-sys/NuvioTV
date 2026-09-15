299e3b797ad959db13784c3e0beff5c17a2912ad
filipempribeiro-sys <filipe.m.p.ribeiro@gmail.com>
2026-09-15 20:58:32 +0100

    design: codify MEDIA-HUB product identity reference

---
+++ mediahub/contracts/app-identity.kt
@@ -0,0 +1,8 @@
+package com.mediahub.contracts
+
+object MediaHubIdentity {
+    const val ProductName = "MEDIA•HUB"
+    const val Descriptor = "TV • FILMES • SÉRIES"
+    const val Slogan = "TODO O ENTRETENIMENTO NUM SÓ LUGAR"
+    const val ApplicationId = "com.mediahub.tv"
+}
